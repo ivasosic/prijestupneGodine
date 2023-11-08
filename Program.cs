cb24f093de7249988996370e6486d34e3bc6074e
a <145933200+ivasosic@users.noreply.github.com>
2023-11-08 08:42:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prijestupneGodine_IvaSosic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PocGod = 1900, ZavGod = 2100;
+            for (int i = PocGod; i <= ZavGod; i++)
+            {
+                if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0))
+                {
+                    Console.WriteLine(i);
+                }
+        }
+
+        Console.ReadKey();
+        }
+    }
+}
